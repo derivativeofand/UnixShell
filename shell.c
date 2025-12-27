0968711df173f936b8991e9f2db71a433df11d6d
derivativeofand <joyha614@gmail.com>
2025-12-27 06:18:18 -0500

    Finished bugs

--- shell.c
+++ shell.c
@@ -1,28 +1,65 @@
 #include "shell.h"
 
-int main(int argc, char* argv[]) {
-    for (int i = 0; i < 20; i++) {
-        printf("*");
+void parseInput(char* input, char** args) {
+    char *token;
+    int i = 0;
+
+    token = strtok(input," \t\n");
+    while(token != NULL && i < MAX_ARGS - 1) {
+        args[i++] = token;
+        token = strtok(NULL, " \t\n");
     }
-    printf("Welcome to my shell");
+
+    args[i] = NULL;
+}
+
+int main(int argc, char* argv[]) {
+    char buffer[MAX_LINE];
+    char *args[MAX_ARGS];
+
+    
     for (int i = 0; i < 20; i++) {
         printf("*");
     }
 
+    printf("\n");
+    
+    printf("Welcome to my shell\n");
+    for (int i = 0; i < 20; i++) {
+        printf("*");
+    }
 
     while(1) {
-        int pid = fork();
+        printf("\nmyShell> ");
 
+        // If EOF or any other issues are encountered, exit the shell
+        if(fgets(buffer, sizeof(buffer), stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+
+        parseInput(buffer, args);
+
+        // Empty command
+        if(args[0] == NULL) {
+            continue; 
+        }
+
+        if(strcmp(args[0], "exit") == 0) {
+            printf("Exiting shell...\n");
+            exit(0);
+        }
+        pid_t pid = fork();
+        
         // If the process is the child process execute the command, else wait for the child process to end
         if(pid == 0) {
-            if(strcmp(argv[0]), "exit" == 0) {
-                exit(0);
-            }
-            execvp(argv[0], argv);
+            execvp(args[0], args);
             perror("Running command failed");
             exit(1);
         } else {
             wait(NULL);
+
         }
     }
+    return 0;
 }
--- shell.h
+++ shell.h
@@ -1,3 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <process.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define MAX_LINE 1024
+#define MAX_ARGS 64
+
+void parseInput(char* input, char** args);
+
+#endif
