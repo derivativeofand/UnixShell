2de8c631012aebcbaf041bf1ec8bb29335f635f0
Junyong Ha <106165436+derivativeofand@users.noreply.github.com>
2025-12-27 01:54:42 -0800

    Finished working on basic operations in shell

---
+++ shell.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+int main(int argc, char* argv[]) {
+    for (int i = 0; i < 20; i++) {
+        printf("*");
+    }
+    printf("Welcome to my shell")
+    for (int i = 0; i < 20; i++) {
+        printf("*");
+    }
+
+
+    while(1) {
+        int pid = fork();
+
+        // If the process is the child process execute the command, else wait for the child process to end
+        if(pid == 0) {
+            if(strcmp(args[0]), "exit" == 0) {
+                exit(0);
+            }
+            execvp(arg[0], argv);
+            perror("Running command failed");
+            exit(1);
+        } else {
+            wait(NULL);
+        }
+    }
+}
